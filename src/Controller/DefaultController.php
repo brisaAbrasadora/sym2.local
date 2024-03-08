7af3b5f23aadbefcb77ee6b8b6b2b98f8e84c7d1
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2024-03-08 23:24:27 +0100

    Testing

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,12 +1,27 @@
 <?php
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     public function index()
     {
-        return $this->render('test.html.twig');
+        $name = 'Sofia';
+        $greeting = 'Good night';
+        $tasks = [
+            'Jumping',
+            'Smiling',
+            'Loving'
+        ];
+        $today = new DateTime();
+
+        return $this->render('test.html.twig', [
+            'name' => $name,
+            'greeting' => $greeting,
+            'tasks' => $tasks,
+            'today' => $today
+        ]);
     }
 }
