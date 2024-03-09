52289bbe4799775bd563f0f82c49289f2f0b2d61
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2024-03-09 15:14:34 +0100

    Data persistence 3.2

--- src/Controller/ImagenController.php
+++ src/Controller/ImagenController.php
@@ -10,13 +10,21 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class ImagenController extends AbstractController
 {
-    #[Route('/imagen', name: 'app_imagen')]
+    #[Route('/galeria', name: 'sym_galeria')]
     public function index(ManagerRegistry $doctrine): Response
     {
         $imagenes = $doctrine->getRepository(Imagen::class)->findAll();
 
         return $this->render('imagen/index.html.twig', [
             'imagenes' => $imagenes,
+        ]);
+    }
+
+    #[Route('/galeria/{id}', name: 'sym_imagen_show')]
+    public function show(Imagen $imagen): Response
+    {
+        return $this->render('imagen/show.html.twig', [
+            'imagen' => $imagen
         ]);
     }
 }
